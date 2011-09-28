bdea7094e53479ef569097223211e9c26760790f
Gentle Trader <ceschiatti@gmail.com>
2011-09-27 23:46:40 -0300

    Now uses a class created in mylib

--- main.cpp
+++ main.cpp
@@ -3,13 +3,19 @@
 #include "myqtapp.h"
 #include <mylib.h>
  
+#include <QDebug>
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     myQtApp *dialog = new myQtApp;
 
 	soma(1,4);
- 
+	
+	MinhaClasse c(3,7);
+	int r = c.multiplica();
+	qDebug() << r;
+
     dialog->show();
 	return app.exec();
 	
